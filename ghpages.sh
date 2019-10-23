e000c67373b07e7176b6d29206019148617b9f3b
qinyu <qinyusuain@gmail.com>
2019-10-24 07:10:18 +0800

    Update gh-pages.

--- ghpages.sh
+++ ghpages.sh
@@ -2,4 +2,16 @@
 
 git checkout gh-pages
 
-rm 
+gauge docs spectacle specs
+
+cp -r ./docs/html/* ./
+
+rm -rf ./docs
+
+git add .
+
+git commit -m "Update gh-pages."
+
+git push origin gh-pages
+
+git checkout master
